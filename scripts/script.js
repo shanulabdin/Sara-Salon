1efcaa324b2daf11962fed6a64e21128f8719e82
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-30 03:19:45 +0500

    open close nav buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 const menu = document.querySelector('.menu');
 const menuToggle = document.querySelector('.menu-toggle');
-
+const menuButton = document.querySelector('.menu-button');
+const closeButton = document.querySelector('.close-button');
 
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('active');
+  menuButton.classList.toggle('active');
+  closeButton.classList.toggle('active');
 })
